b17437d47c8c4d148c79e98dd3972397820b91dd
Katy Link <ktelink@gmail.com>
2020-02-25 11:36:32 +0100

    creating user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+
+User.destroy_all if Rails.env.development?
+
+puts 'Creating users'
+
+user_1 = User.new(email: "katy@gmail.com", password: "123456")
+user_1.save!
+user_2 = User.create(email: "laura@gmail.com", password: "123457")
+user_3 = User.create(email: "meghan@gmail.com", password: "123458")
+user_4 = User.create(email: "kendrikc@gmail.com", password: "123459")
+
+puts "#{User.count} users created."
+
+# Plane.destroy_all if Rails.env.development?
+
+# puts 'Creating airplanes'
+
+# plane_1 = Plane.create(capacity: 10, location: "Rio de Janeiro", type: "Boing 123", pilot_name: "Bruce", rate: 1000, description: "super stylish vintage model", user_id: 1 )
+# plane_2 = Plane.create(capacity: 6, location: "Tokyo", type: "Airbus 333", pilot_name: "Leslie", rate: 1500, description: "futuristic look", user_id: 2 )
+
+# puts "#{Plane.count} planes created."
+
+
+
+
+
