9acf128e6d3af299c1bd9dd4b53284166ec3d0dc
Katy Link <ktelink@gmail.com>
2020-02-25 17:51:44 +0100

    button design and login page

--- db/migrate/20200224160307_devise_create_users.rb
+++ db/migrate/20200224160307_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
-      ## Rememberable
+      ## Rememberabl
       t.datetime :remember_created_at
 
       ## Trackable
