4d264598cebdbe66eb74184c67966ef40c5c4dfe
Katy Link <ktelink@gmail.com>
2020-02-26 15:51:38 +0100

    validations added to plane submit form

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -27,7 +27,7 @@ class PlanesController < ApplicationController
   private
 
   def plane_params
-    params.require(:plane).permit(:capacity, :location, :type, :rate, :description, :pilot_name, :photo)
+    params.require(:plane).permit(:capacity, :location, :name, :rate, :description, :pilot_name, :photo)
   end
 
 
