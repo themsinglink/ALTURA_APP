b139c20fa6f579e3a0ee83cd338dbebda48fe15d
Meghan Martin <meghanxmartin@gmail.com>
2020-02-28 11:58:09 +0100

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
 
   def dashboard
+    @planes = Plane.all
+    if User.planes.any?
+      render '/dashboard'
+    else
+      render '/trips'
+    end
   end
 end
